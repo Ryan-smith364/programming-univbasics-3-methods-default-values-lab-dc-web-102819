b098622ddebf8fcb5f135dbcb31290f7d417e4ec
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-25 03:02:03 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
 def meal_choice ( veg1, veg2, protein = "meat")
   puts "What a nutritious meal!".to_s
-  puts  "A plate of #{protein} with #{veg1} and #{veg2}.".to_s
-  
+  plate =  "A plate of #{protein} with #{veg1} and #{veg2}.".to_s
+  puts plate
   
 end
 
